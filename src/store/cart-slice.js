212829ab232b3f9eecd3fab2fde0d319bc93eef1
Dmitry Kovalkov <dmitriy.k@kultprosvet.net>
2025-10-16 11:17:03 +0300

    226. Работа с Асинхронным Кодом в Redux / 227. Frontend vs Backend / 228. Размещение Логики

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -38,6 +38,10 @@ const cartSlice = createSlice({
         existingItem.quantity--;
         existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
       }
+    },
+    updateCart: (state, action) => {
+      state.items = action.payload.items;
+      state.itemsQuantity = action.payload.itemsQuantity;
     }
   }
 });
